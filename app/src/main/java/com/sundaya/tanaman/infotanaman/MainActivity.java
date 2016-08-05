8208764ec8ea5efeec0761c4ac3d886309e22542
asiku <kuprabelantuk@gmail.com>
2016-08-05 16:39:46 +0700

    tes wa

---
+++ app/src/main/java/com/sundaya/tanaman/infotanaman/MainActivity.java
@@ -0,0 +1,44 @@
+package com.sundaya.tanaman.infotanaman;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        setContentView(R.layout.activity_main);
+
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+
+}
---
+++ app/src/main/java/com/sundaya/tanaman/infotanaman/ProportionalImageView.java
@@ -0,0 +1,36 @@
+package com.sundaya.tanaman.infotanaman;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Userpc on 04/08/2016.
+ */
+
+public class ProportionalImageView extends ImageView {
+
+    public ProportionalImageView(Context context) {
+        super(context);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable d = getDrawable();
+        if (d != null) {
+            int w = MeasureSpec.getSize(widthMeasureSpec);
+            int h = w * d.getIntrinsicHeight() / d.getIntrinsicWidth();
+            setMeasuredDimension(w, h);
+        }
+        else super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
